8d4ab724708217643420f32229b8244a9c68315a
Jordan Wiens <github@psifertex.com>
2025-09-26 11:38:11 -0400

    allow not replacing the readme

--- do_release.py
+++ do_release.py
@@ -8,14 +8,15 @@ from os import path
 from argparse import ArgumentParser
 from subprocess import run
 from pathlib import Path
-'''WARNING: IF YOU DO NOT UPDATE YOUR README.md USING generate_plugininfo.py 
-THIS PLUGIN WILL OVERWRITE IT!'''
+'''WARNING: BY DEFAULT THIS PLUGIN WILL REGENERATE YOUR README.md! 
+Use --skip-regen-readme to only update the version number in README.md.'''
 
 parser = ArgumentParser()
 parser.add_argument("-d", "--description", help="Description for the new release", action="store", dest="description", default="")
 parser.add_argument("-s", "--subdir", help="Specify a subdirectory", action="store", dest="subdir", default="")
 parser.add_argument("-v", "--version", help="New version string", action="store", dest="new_version", default="")
 parser.add_argument("--force", help="Override the repository dirty check", action="store_true", dest="dirtyoverride", default=False)
+parser.add_argument("--skip-regen-readme", help="Skip regenerating README.md, only update version number", action="store_true", dest="skip_regen_readme", default=False)
 args = parser.parse_args()
 #TODO
 
@@ -43,9 +44,46 @@ def update_version(data):
 	print(f"Updating plugin with new version {data['version']}")
 	with open(pluginfile, 'w') as plugin:
 		dump(data, plugin, indent=4)
-	run([generator, "-r", "-f"], check=True)
+	
+	readme_path = 'README.md'
+	
+	if args.skip_regen_readme:
+		# Only update the version number in the README without regenerating it
+		import re
+		if path.exists(readme_path):
+			print("Updating only version number in README.md without regenerating")
+			with open(readme_path, 'r') as f:
+				readme_content = f.read()
+			
+			# Update version in the title (format example: # Plugin Name (vX.Y.Z))
+			old_version = None
+			for line in readme_content.split('\n'):
+				if line.startswith('# ') and ' (v' in line:
+					match = re.search(r'\(v([\d\.]+)\)', line)
+					if match:
+						old_version = match.group(1)
+						break
+			
+			if old_version:
+				print(f"Replacing version {old_version} with {data['version']} in README.md")
+				updated_content = re.sub(
+					r'(# .+?\(v)[\d\.]+(\))',
+					f'\\1{data["version"]}\\2',
+					readme_content
+				)
+				
+				with open(readme_path, 'w') as f:
+					f.write(updated_content)
+				
+				repo.index.add(readme_path)
+			else:
+				print("Warning: Could not find version pattern in README.md")
+	else:
+		# Full README regeneration
+		run([generator, "-r", "-f"], check=True)
+		repo.index.add(readme_path)
+	
 	repo.index.add(pluginfile)
-	repo.index.add('README.md')
 	if args.description == "":
 		repo.index.commit(f"Updating to {data['version']}")
 	else:
